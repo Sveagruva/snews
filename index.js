5f6464c7b21231b292146a3f358ad6ef0ba2562e
Sveagruva <55844843+Sveagruva@users.noreply.github.com>
2021-05-03 16:56:19 +0400

    add about page and remove comments from generated html

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const p = require('path');
 
-const copmsPath = p.join(__dirname, 'comp');
-
 const header = fs.readFileSync(p.join(__dirname, 'comp' , 'header.html'), 'utf-8');
 const footer = fs.readFileSync(p.join(__dirname, 'comp', 'footer.html'), 'utf-8');
 
@@ -15,6 +13,8 @@ const create = (from, to) => {
     let html = header + fs.readFileSync(from, 'utf8') + footer;
     let vars = JSON.parse(fs.readFileSync(p.join(p.dirname(from), 'vars.json'), 'utf-8'));
 
+
+    html = html.replace(/<\!--.*?-->/mgis, "");
     let matches = html.match(/{{{.*?}}}/mgi);
     // I prefer lighter
 
