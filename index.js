564cc51aabbadfb58c3ef1055d67bd7b7be79865
Sveagruva <55844843+Sveagruva@users.noreply.github.com>
2021-06-27 15:33:22 +0400

    refactoring and default title

--- index.js
+++ index.js
@@ -2,29 +2,31 @@ const fs = require('fs');
 const p = require('path');
 const fse = require('fs-extra');
 
-
 const buildDir = p.join(__dirname, 'build');
 if(!fs.existsSync(buildDir))
     fs.mkdirSync(buildDir);
 
+
+
 const header = fs.readFileSync(p.join(__dirname, 'comp' , 'header.html'), 'utf-8');
 const footer = fs.readFileSync(p.join(__dirname, 'comp', 'footer.html'), 'utf-8');
 
+const globalsVars = {
+    title: 'SNEWS',
+};
+
+const copyObj = obj => JSON.parse(JSON.stringify(obj));
+
 
 const create = (from, to) => {
-    // console.log(from, to);
-
     fs.mkdirSync(p.dirname(to), { recursive: true });
 
     let html = header + fs.readFileSync(from, 'utf8') + footer;
-    let vars = JSON.parse(fs.readFileSync(p.join(p.dirname(from), 'vars.json'), 'utf-8'));
+    let vars = Object.assign(copyObj(globalsVars), JSON.parse(fs.readFileSync(p.join(p.dirname(from), 'vars.json'), 'utf-8')));
 
-
-    html = html.replace(/<\!--.*?-->/mgis, "");
-    let matches = html.match(/{{{.*?}}}/mgi);
-    // I prefer lighter
-
-    matches.forEach(match => {
+    // remove html comments
+    html = html.replace(/<\!--.*?-->/mgis, '');
+    html.match(/{{{.*?}}}/mgi).forEach(match => {
         let variableName = match.slice(3, -3);
         let value = '';
 
@@ -39,9 +41,7 @@ const create = (from, to) => {
 
 
 const sendAll = (from, abs) => {
-    let all = fs.readdirSync(from);
-
-    all.forEach(element => {
+    fs.readdirSync(from).forEach(element => {
         if(element.endsWith('.json')) return;
 
         if(element.endsWith('.html'))
